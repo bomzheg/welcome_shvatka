47aba14ec589a03aa3ca54d6ecb5db0cdf2663d0
Yuriy Chebyshev <bomzheg@gmail.com>
2023-05-18 12:57:13 +0300

    text fixes

--- app/keyboadrs.py
+++ app/keyboadrs.py
@@ -4,7 +4,8 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 def get_start_kb() -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
-    builder.button(text="Хочу попробовать сыграть в Схватку")
+    builder.button(text="Хочу сыграть в Схватку")
     builder.button(text="Хочу узнать больше о Схватке")
     builder.button(text="Я уже играл в Схватку раньше, хочу вернуться")
+    builder.adjust(1)
     return builder.as_markup(resize_keyboard=True)
